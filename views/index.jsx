6ec120076a4b90505527e8b78eb57f86d8e518bc
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2022-08-03 10:15:08 -0400

    index.jsx view rendered at pokemon route
    myStyle was added

--- views/index.jsx
+++ views/index.jsx
@@ -2,8 +2,12 @@ const React = require('react')
 class Index extends React.Component{
     render() {
         const { pokemon } = this.props
+        const myStyle = {
+            color: '#ffffff',
+            backgroundColor: '#000000',
+          }
         return (
-            <div>
+            <div style={myStyle}>
                 <h1>See All the Pokemon</h1>
             </div>
         )
