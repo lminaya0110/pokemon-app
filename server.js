2caa4e4249b86bc1929fd23e87534f128606d43d
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2022-08-04 09:29:35 -0400

    deploying app

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const port = 3000
 
 
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
 mongoose.connection.once('open', ()=> {
     console.log('connected to mongo');
 });
